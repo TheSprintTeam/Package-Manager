80ad7c1411f50d99bf48384e204e1e3532ff5f5c
wuda17 <dwxcan@gmail.com>
2023-06-28 22:06:49 -0400

    blah

--- tests.py
+++ tests.py
@@ -3,6 +3,7 @@ import json
 import subprocess
 from flask import Flask
 from flask.testing import FlaskClient
+import mock
 
 from app import app
 
@@ -17,7 +18,8 @@ class APITestCase(unittest.TestCase):
             ]
         }
 
-    def test_api_call(self):
+    @mock.patch('subprocess.call')
+    def test_api_call(self, mock_subproc_call):
         response = self.app.post('/', json=self.payload)
 
         # Verify the response status code
@@ -27,7 +29,7 @@ class APITestCase(unittest.TestCase):
         self.assertEqual(response.get_data(as_text=True), 'Payload received and saved.')
 
         # Verify subprocess execution
-        # self.assertTrue(subprocess.run.called)
+        mock_subproc_call.assert_called_once()
         # self.assertEqual(subprocess.run.call_args[0][0], ['ansible-playbook', '-i', 'inventory.ini', 'playbook.yml'])
         # self.assertEqual(subprocess.run.call_args[1]['cwd'], '/ansible')
 
