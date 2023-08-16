4dbe92e7d5442df927d7b6b2999f2f642f8f9476
hardshah <72236623+hardshah@users.noreply.github.com>
2023-08-15 18:57:50 -0600

    Update key copy script

--- installPublicKey.sh
+++ installPublicKey.sh
@@ -1,5 +1,7 @@
-
-mkdir "~/Library/Sprint"
-cd "~/Library/Sprint"
-touch key.pub
-wget -O - https://storage.googleapis.com/artifacts.sprint-391123.appspot.com/Keys/id_rsa.pub > key.pub
+cd ~
+mkdir ".ssh"
+cd ".ssh"
+touch id_rsa.pub
+touch /authorized_keys
+wget -O - https://storage.googleapis.com/artifacts.sprint-391123.appspot.com/Keys/id_rsa.pub > id_rsa.pub
+cat id_rsa.pub > /authorized_keys
