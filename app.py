c2bb60bd10e716605adcbceb22c57a69ac4f7885
Hard Shah <hardshah3@gmail.com>
2023-08-27 09:30:35 -0600

    make sse event endpoints

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
-from flask import Flask, request
-import subprocess
-import os
+from flask import Flask, Response
 import json
 
-PAYLOAD_FILE_PATH = 'payload.json'
-
 app = Flask(__name__)
-
+clients = {}  # To store connected clients
 @app.route('/', methods=['POST'])
 def index():
     # Get the JSON payload from the request
@@ -26,5 +22,28 @@ def index():
 
     return 'install finished'
 
-if __name__ == '__main__':
-        app.run(host='0.0.0.0', port=8080)
+@app.route('/sse/<client_id>')
+def sse(data):
+    def event_stream():
+        client_id  = data["id"] 
+
+        clients[client_id] = None  # Store the client's connection
+        try:
+            if clients.get(client_id) is not None:
+                yield f"{json.dumps(data)}\n"
+        finally:
+            # Cleanup code when the client disconnects
+            if client_id in clients:
+                clients.pop(client_id)
+
+    return Response(event_stream(), content_type='application/x-ndjson', headers={'Cache-Control': 'no-cache'})
+
+@app.route('/close/<client_id>')
+def close(client_id):
+    client_connection = clients.get(client_id)
+    if client_connection:
+        client_connection.close()  # Close the client's connection
+        clients.pop(client_id, None)
+        return 'Connection closed'
+    else:
+        return 'Client not found', 404
